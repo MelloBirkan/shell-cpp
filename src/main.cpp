1ae68d2b9d9880feb897f9a45fef7b3724f90112
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-01-13 12:10:23 -0300

    Enhance command handling in main loop by adding 'echo' functionality and improving input parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-      std::string input;
 
   while (true) {
     std::cout << "$ ";
 
+    std::string input;
     std::getline(std::cin, input);
+
+    std::stringstream ss(input);
+    std::string comando;
+    std::string argumento;
+    ss >> comando;
+    std::getline(ss >> std::ws, argumento);
     
     if (input == "exit 0") {
       break;
+    } else if (comando == "echo") {
+      std::cout << argumento << "\n";
+    } else {
+      std::cout << input <<": command not found\n";
     }
-    
-    std::cout << input <<": command not found\n";
   }
   return 0;
 }
+
