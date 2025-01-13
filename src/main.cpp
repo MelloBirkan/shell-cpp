65b2c635cc7e66d6d351702e0c142b5a4629853a
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-01-13 11:48:55 -0300

    Implement exit command in main loop to allow graceful termination of the program

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,18 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+      std::string input;
 
   while (true) {
     std::cout << "$ ";
-    
 
-    std::string input;
     std::getline(std::cin, input);
+    
+    if (input == "exit 0") {
+      break;
+    }
+    
     std::cout << input <<": command not found\n";
   }
+  return 0;
 }
